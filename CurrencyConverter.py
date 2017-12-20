b0091d9e97c632c00628c77d94f1c72d32cfb54d
Tejasvi Jodhka <tjodhka@peraso-corp.perasotech.com>
2017-12-20 15:36:42 -0500

    Improve usage of cacheing in CurrencyConverter so that it checks both base and target currency

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -20,34 +20,60 @@ def _cache(currency, response):
 
 def _get_cached(currency):
     if currency in cache and datetime.today() - cache[currency].retrieved < cache_timeout:
-        return cache[currency].response
+        return cache[currency].response['rates']
     return None
 
 
 def _get_rates(currency_base):
-    cached_response = _get_cached(currency_base)
-    if cached_response is not None:
-        response = cached_response
-    else:
-        url = url_base + 'latest?base=' + currency_base
-        r = requests.get(url, headers=headers)
-        response = r.json()
-        _cache(currency_base, response)
+    url = url_base + 'latest?base=' + currency_base
+    r = requests.get(url, headers=headers)
+    response = r.json()
+    _cache(currency_base, response)
     return response['rates']
 
 
 def convert(value, currency, target):
-    rates = _get_rates(currency)
-    if target in rates:
-        return value * rates[target]
-    elif target == currency:
-        return value
+    currency_cached = _get_cached(currency)
+    target_cached = _get_cached(target)
+
+    if currency_cached is not None:
+        if target in currency_cached:
+            return value * currency_cached[target]
+        else:
+            raise Exception("Currency not found")
+    elif target_cached is not None:
+        if currency in target_cached:
+            return value / target_cached[currency]
+        else:
+            raise Exception("Currency not found")
     else:
-        raise Exception("Currency not found")
+        rates = _get_rates(currency)
+        if target in rates:
+            return value * rates[target]
+        elif target == currency:
+            return value
+        else:
+            raise Exception("Currency not found")
 
 
 def _main():
+    # caching test
+    convert(3599, "USD", "CAD")
+    convert(3599, "USD", "CAD")
+    convert(3599, "USD", "CAD")
+    convert(3599, "USD", "CAD")
+    convert(3599, "USD", "CAD")
+    convert(3599, "USD", "CAD")
+    convert(3599, "USD", "CAD")
+    convert(3599, "USD", "CAD")
     print convert(3599, "USD", "CAD")
+
+    convert(4624.715, "CAD", "USD")
+    convert(4624.715, "CAD", "USD")
+    convert(4624.715, "CAD", "USD")
+    convert(4624.715, "CAD", "USD")
+    convert(4624.715, "CAD", "USD")
+    print convert(4624.715, "CAD", "USD")
 
 
 if __name__ == "__main__":
